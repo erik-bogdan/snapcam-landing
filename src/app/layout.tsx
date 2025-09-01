62cc3f17f15246d30b569cdd48057c09b9c77422
Erik Bogdan <erik.bogdan@verycreatives.com>
2025-09-01 19:36:17 +0200

    Updated icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { CookieConsent } from "@/components/ui/cookie-consent";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,45 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "SnapCam - Forradalmi Képmegosztás",
   description: "Magyarország első interaktív platformja, ahol az emlékek élnek",
+  manifest: "/images/favico/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/images/favico/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/images/favico/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" }
+    ],
+    apple: [
+      { url: "/images/favico/apple-touch-icon.png", sizes: "180x180", type: "image/png" }
+    ],
+    shortcut: [
+      { url: "/favicon.ico" }
+    ]
+  },
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || "https://snapcam.hu"),
+  openGraph: {
+    title: "SnapCam - Forradalmi Képmegosztás",
+    description:
+      "Magyarország első interaktív platformja, ahol az emlékek élnek",
+    url: "/",
+    siteName: "SnapCam",
+    type: "website",
+    locale: "hu_HU",
+    images: [
+      {
+        url: "/ogimg.png",
+        width: 1200,
+        height: 630,
+        alt: "SnapCam - Forradalmi Képmegosztás",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SnapCam - Forradalmi Képmegosztás",
+    description:
+      "Magyarország első interaktív platformja, ahol az emlékek élnek",
+    images: ["/ogimg.png"],
+  },
 };
 
 export default async function RootLayout({
@@ -49,6 +89,7 @@ export default async function RootLayout({
         {children}
         <Toaster />
         <CookieConsent />
+        <Analytics/>
       </body>
     </html>
   );
